67ad596f85d7d788b2fc974d48219855945bede8
KANTIN-FAGN <kantin.fagniart@ynov.com>
2024-04-18 17:44:00 +0200

    add architecture

---
+++ Backend/app.js
@@ -0,0 +1,8 @@
+////////////////////////////////////////////////////////////////
+
+// npm init -> créer le package.json
+// npm start -> pour le lancement du serveur
+// npm install express -> install express
+// npm i nodemon --save-dev -> pour rafraichir le serveur sans le relancer (juste pour la phase de dev (dans package.json) )
+
+////////////////////////////////////////////////////////////////
