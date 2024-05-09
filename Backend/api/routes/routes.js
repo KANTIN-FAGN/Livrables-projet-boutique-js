d880a699a9d7d9abc4cc67b4fdce25631a5d726c
KANTIN-FAGN <kantin.fagniart@ynov.com>
2024-05-09 23:12:43 +0200

    Closes #1

--- Backend/api/routes/routes.js
+++ Backend/api/routes/routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middlewares/auth')
 const controllers = require('../controllers/articles');
 
 //Define article's routes
 router.get('/article/:id', controllers.getArticle);
-router.get('/articles-homme/', controllers.getArticleHomme);
-router.get('/articles-femme/', controllers.getArticleFemme);
+router.get('/mode-homme/', controllers.getArticleHomme);
+router.get('/mode-femme/', controllers.getArticleFemme);
 
 //important pour le app.use dans le serveur
 module.exports = router;
---
+++ Frontend/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const routes = require('./routes/routes')
+
+// EJS
+app.set('view engine', 'ejs')
+app.use("/public/", express.static('assets'));
+app.use(cors())
+app.use(routes)
+
+const port = 3000
+
+app.listen(port, '0.0.0.0', console.log(`Server Frontend lancé sur => http://localhost:${port}/index`));
