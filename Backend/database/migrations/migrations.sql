10a20f02ef809d3ed8290be8eb3d8c533a15206d
KANTIN-FAGN <kantin.fagniart@ynov.com>
2024-05-22 10:55:05 +0200

    ajout responsive

--- Backend/database/migrations/migrations.sql
+++ Backend/database/migrations/migrations.sql
@@ -1,32 +1,32 @@
-CREATE TABLE color(
+CREATE TABLE color (
    id_color SMALLINT AUTO_INCREMENT,
    color VARCHAR(50) NOT NULL,
-   PRIMARY KEY(id_color),
-   UNIQUE KEY `color` ("color"),
+   PRIMARY KEY (id_color),
+   UNIQUE (color)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE material(
+CREATE TABLE material (
    id_material SMALLINT AUTO_INCREMENT,
    material VARCHAR(50) NOT NULL,
-   PRIMARY KEY(id_material),
-   UNIQUE KEY `material` ("material"),
+   PRIMARY KEY (id_material),
+   UNIQUE (material)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE size(
+CREATE TABLE size (
    id_size SMALLINT AUTO_INCREMENT,
    size VARCHAR(50) NOT NULL,
-   PRIMARY KEY(id_size),
-   UNIQUE KEY `size` ("size"),
+   PRIMARY KEY (id_size),
+   UNIQUE (size)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE category(
+CREATE TABLE category (
    id_category SMALLINT AUTO_INCREMENT,
    category VARCHAR(100) NOT NULL,
-   PRIMARY KEY(id_category),
-   UNIQUE KEY `category` ("category"),
+   PRIMARY KEY (id_category),
+   UNIQUE (category)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE article(
+CREATE TABLE article (
    id_article SMALLINT AUTO_INCREMENT,
    name VARCHAR(150) NOT NULL,
    detail VARCHAR(50) NOT NULL,
@@ -36,40 +36,40 @@ CREATE TABLE article(
    reduction SMALLINT NOT NULL,
    genders VARCHAR(50) NOT NULL,
    id_category SMALLINT NOT NULL,
-   PRIMARY KEY(id_article),
-   FOREIGN KEY(id_category) REFERENCES category(id_category),
+   PRIMARY KEY (id_article),
+   FOREIGN KEY (id_category) REFERENCES category (id_category)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE image(
+CREATE TABLE image (
    id_image SMALLINT AUTO_INCREMENT,
    img VARCHAR(250) NOT NULL,
    id_article SMALLINT NOT NULL,
-   PRIMARY KEY(id_image),
-   FOREIGN KEY(id_article) REFERENCES article(id_article),
-   UNIQUE KEY `img` ("img"),
+   PRIMARY KEY (id_image),
+   FOREIGN KEY (id_article) REFERENCES article (id_article),
+   UNIQUE (img)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE have_colors(
+CREATE TABLE have_colors (
    id_article SMALLINT,
    id_color SMALLINT,
-   PRIMARY KEY(id_article, id_color),
-   FOREIGN KEY(id_article) REFERENCES article(id_article),
-   FOREIGN KEY(id_color) REFERENCES color(id_color),
+   PRIMARY KEY (id_article, id_color),
+   FOREIGN KEY (id_article) REFERENCES article (id_article),
+   FOREIGN KEY (id_color) REFERENCES color (id_color)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE have_materials(
+CREATE TABLE have_materials (
    id_article SMALLINT,
    id_material SMALLINT,
-   PRIMARY KEY(id_article, id_material),
-   FOREIGN KEY(id_article) REFERENCES article(id_article),
-   FOREIGN KEY(id_material) REFERENCES material(id_material),
+   PRIMARY KEY (id_article, id_material),
+   FOREIGN KEY (id_article) REFERENCES article (id_article),
+   FOREIGN KEY (id_material) REFERENCES material (id_material)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE have_sizes(
+CREATE TABLE have_sizes (
    id_article SMALLINT,
    id_size SMALLINT,
    stock SMALLINT NOT NULL,
-   PRIMARY KEY(id_article, id_size),
-   FOREIGN KEY(id_article) REFERENCES article(id_article),
-   FOREIGN KEY(id_size) REFERENCES size(id_size),
+   PRIMARY KEY (id_article, id_size),
+   FOREIGN KEY (id_article) REFERENCES article (id_article),
+   FOREIGN KEY (id_size) REFERENCES size (id_size)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
---
+++ Frontend/assets/js/menu-burger.js
@@ -0,0 +1,34 @@
+const menuHamburger = document.querySelector(".menu-hamburger");
+const navLinks = document.querySelector(".nav-links");
+const Overlay = document.querySelector(".overlay");
+const html = document.querySelector("html")
+
+menuHamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('mobile-menu');
+  menuHamburger.classList.toggle('active');
+  Overlay.classList.toggle('active-overlay');
+  html.classList.toggle('overflow')
+});
+
+// fonction changement de sexe menu burger
+const homme = document.getElementById('homme');
+const femme = document.getElementById('femme');
+const listHomme = document.getElementById('list-homme');
+const listFemme = document.getElementById('list-femme');
+
+const toggleGender = (selected, notSelected, showList, hideList) => {
+  selected.classList.add('select-sex');
+  notSelected.classList.remove('select-sex');
+  selected.classList.remove('not-select-sex');
+  notSelected.classList.add('not-select-sex');
+  showList.classList.add('display-block');
+  hideList.classList.remove('display-block');
+  hideList.classList.add('masque-sex');
+};
+
+homme.addEventListener('click', () => toggleGender(homme, femme, listHomme, listFemme));
+femme.addEventListener('click', () => toggleGender(femme, homme, listFemme, listHomme));
+
+// Initial state setup
+listHomme.classList.add('display-block');
+listFemme.classList.add('masque-sex');
