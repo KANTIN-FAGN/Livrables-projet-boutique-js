b9e1a1137f3e515bb3b6890ed965192a2679606e
KANTIN-FAGN <kantin.fagniart@ynov.com>
2024-05-27 17:22:54 +0200

    ajout du front homme & femme

--- Frontend/controller/controller.js
+++ Frontend/controller/controller.js
@@ -1,4 +1,37 @@
+const url = "http://localhost:4000/";
+
 exports.Index = (req, res) => {
     res.render('../views/pages/index')
 }
 
+exports.ArticlesHomme = (req, res) => {
+    getArticlesHomme()
+        .then((articles) => {
+            res.render("../views/pages/articles", {
+                lst: articles.articles.items
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
+exports.ArticlesFemme = (req, res) => {
+    getArticlesFemme()
+        .then((articles) => {
+            res.render("../views/pages/articles", {
+                lst: articles.articles.items
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
+
+async function getArticlesHomme() {
+    const data = await fetch(`${url}mode-homme/`);
+    return data.json();
+}
+async function getArticlesFemme() {
+    const data = await fetch(`${url}mode-femme/`);
+    return data.json();
+}
--- Frontend/routes/routes.js
+++ Frontend/routes/routes.js
@@ -3,5 +3,7 @@ const router = express.Router()
 const controller = require('../controller/controller')
 
 router.get('/Roid', controller.Index)
+router.get('/Roid/mode-homme', controller.ArticlesHomme)
+router.get('/Roid/mode-femme', controller.ArticlesFemme)
 
 module.exports = router
