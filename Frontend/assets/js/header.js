0e890126c257fc7f9dadeb3956763f4f0418bf5a
KANTIN-FAGN <kantin.fagniart@ynov.com>
2024-06-04 19:41:48 +0200

    Closes #14

--- Frontend/assets/js/header.js
+++ Frontend/assets/js/header.js
@@ -40,3 +40,18 @@ iconCloseHeaderSlider.addEventListener('click', () => {
     iconSilderCompte.classList.remove('active');
     document.body.classList.remove("no-scroll");
 });
+
+const backToConnect = document.querySelector('.backToConnect');
+const forgotPwd = document.querySelector('.forget-pwd');
+
+const connect = document.querySelector('.connexion-input-global');
+const forgot = document.querySelector('.forgot-pwd-global');
+
+forgotPwd.addEventListener('click', () => {
+    forgot.classList.add('active');
+    connect.classList.add('remove');
+})
+backToConnect.addEventListener('click', () => {
+    forgot.classList.remove('active');
+    connect.classList.remove('remove');
+})
